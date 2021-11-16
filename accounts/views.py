3c429c4c320217df524446a6c4e351daab48c51c
eunseo130 <kys980130@naver.com>
2021-11-16 17:50:56 +0900

    Update Accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
 # Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+def signup(request):
+    password = request.data.get('password')
+    password_confirm = request.data.get('passwordConfirm')
+    if password != password_confirm:
+        error_message = {
+            'error': '비밀번호가 일치하지 않습니다.'
+        }
+        return Response(error_message, status=status.HTTP)
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(password)
+        user.save()
+        return Response(data=serializer.data)
+
+
+def profile(request, username):
+    profile = get_object_or_404(User, username=username)
+    context = {
+        'profile': profile,
+    }
+    return render(request, 'accounts/profile.html', context)
--- community/views.py
+++ community/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from community.models import Article
+from .models import Movie, Article
 
 # Create your views here.
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('pk', 'title')
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('pk', 'title')
+    
+    movie_set = MovieSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Article
+        fields = ('pk', 'title', 'content', 'movie_set',)
